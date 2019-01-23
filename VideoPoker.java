2bf7cffafb9bfc1391908342ddd01713c29a6973
Kedar Gangopadhyay <skg2016@nyu.edu>
2019-01-23 01:14:22 -0500

    VideoPoker

--- VideoPoker.java
+++ VideoPoker.java
@@ -2,9 +2,8 @@
 */
 
 public class VideoPoker {
-
 	public static void main(String[] args) {
-			Game g = new Game();
-			g.play();
+		Game g = new Game();
+		g.play();
 	}
 } // end class VideoPoker
